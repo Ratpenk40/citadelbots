6633288a200a6c7353f22b66324cd16b6ab22f76
alexandrev40 <alexandre.vaudano@nagibio.ch>
2026-08-23 16:22:07 +0200

    Thief, assassin, condottiere

--- characters.py
+++ characters.py
@@ -1,4 +1,25 @@
-import numpy as np
+from random import shuffle
+
+def poll_for_index(possible_values, poll_type):
+    poll = f"Press key to select {poll_type} : "
+    while True:
+        pressed_key = input(poll)
+        if pressed_key.isdigit() and int(pressed_key) < len(possible_values):
+            return int(pressed_key)
+        print('Please type a valid number')
+
+class Action:
+    def __init__(self, name, callback, description=""):
+        self.name = name
+        self.callback = callback
+        self.description = description
+
+    def play(self):
+        return self.callback()
+
+    def __str__(self):
+        return self.name
+
 class Character:
     def __init__(self, name, player, color=None, play_order=0):
         self.name = name
@@ -7,32 +28,48 @@ class Character:
         self.play_order = play_order
 
     def __str__(self):
-        print(self.name)
+        return self.name
 
     def collect_color_money(self):
         if self.color is not None:
-            for dist in self.player.played_district:
+            for dist in self.player.played_districts:
                 if dist.color == self.color:
                     self.player.money += 1
-    
+
+    def add_action(self, name, callback, description=""):
+        action = Action(name, callback, description)
+        self.player.available_actions.append(action)
 
 class Assassin(Character):
-    def __init__(self, name="Assassin"):
-        super().__init__(name, play_order=0)
+    def __init__(self, player, name="Assassin"):
+        super().__init__(name, player, play_order=0)
 
-    def kill_other_character(self, character_list):
-        pass
+    def kill_other_character(self):
+        char_list =CHARACTER_CLASSES.remove(Assassin)
+        for i, character in enumerate(char_list):
+            print(f"{i}: {character.name}")
 
+        target_index = poll_for_index(char_list, "player")
+        self.player.game.assassinated = char_list[target_index].name
+        print(f"The assassin...")
 class Thief(Character):
-    def __init__(self, name='Thief'):
-        super().__init__(name, play_order=1)
+    def __init__(self, player, name='Thief'):
+        super().__init__(name, player, play_order=1)
 
-    def steal_other_character(self, character_list):
-        pass
+    def steal_other_character(self):
+        char_list =  CHARACTER_CLASSES.remove(Assassin)
+        char_list = char_list.remove(self.player.game.assassinated)
+        char_list = char_list.remove(Thief)
+        for i, character in enumerate(char_list):
+            print(f"{i}: {character.name}")
+        target_index = poll_for_index(char_list, "player")
+        self.player.game.stolen = char_list[target_index].name
+        print("The thief will steal")
+
 
 class Wizard(Character):
-    def __init__(self, name='Wizard'):
-        super().__init__(name, play_order=2)
+    def __init__(self, player, name='Wizard'):
+        super().__init__(name, player, play_order=2)
 
     def swap_cards(self, player_list):
         pass
@@ -41,51 +78,148 @@ class King(Character):
     def __init__(self, player, name='King', color='Y', play_order=3):
         super().__init__(name, player, color, play_order)
 
+        self.add_action(
+            "Collect yellow district income",
+            self.collect_color_money
+        )
+
 class Bishop(Character):
     def __init__(self, player, name='Bishop', color="B", play_order=4):
         super().__init__(name, player, color, play_order)
+
+        self.add_action(
+            "Collect blue district income",
+            self.collect_color_money
+        )
 
 class Merchant(Character):
     def __init__(self,player, name='Merchant', color="G", play_order=5):
         super().__init__(name, player, color, play_order)
 
+        self.add_action(
+            "Collect green district income",
+            self.collect_color_money
+        )
+        self.player.money += 1
+
+
 class Architect(Character):
     def __init__(self, player,name='Architect', color=None, play_order=6):
         super().__init__(name, player, color, play_order)
 
+    def architect_powers(self):
+        cards_drew = [self.player.deck.draw_card() for _ in range(2)]
+        self.player.cards.extend(cards_drew)
+        self.player.available_actions.extend(['play_district','play_district'])
+
+
 class Condottiere(Character):
     def __init__(self,  player,name='Condottiere', color='R', play_order=7):
         super().__init__(name, player, color, play_order)
+
+        self.add_action(
+            "Collect red district income",
+            self.collect_color_money
+        )
+        self.add_action(
+            "Destroy a district",
+            self.destroy_district
+        )
+
+    def destroy_district(self):
+        if self.player.game is None:
+            print("Condottiere needs a game with players to destroy a district.")
+            return
+
+        other_players = [
+            player
+            for player in self.player.game.players
+            if player is not self.player
+            and player.played_districts
+            and not player.city_completed
+            and not (
+                player.character is not None
+                and player.character.name == "Bishop"
+            )
+        ]
+        possible_targets = [
+            player
+            for player in other_players
+            if any(district.value - 1 <= self.player.money for district in player.played_districts)
+        ]
+
+        if not possible_targets:
+            print("No district can be destroyed.")
+            return
+
+        print("Select player:")
+        for i, target in enumerate(possible_targets):
+            print(f"{i}: {target.player_name}")
+        target_index = poll_for_index(possible_targets, "player")
+        target = possible_targets[target_index]
+
+        possible_districts = [
+            district
+            for district in target.played_districts
+            if district.value - 1 <= self.player.money
+        ]
+        print("Select district to destroy:")
+        for i, district in enumerate(possible_districts):
+            destruction_cost = max(0, district.value - 1)
+            print(f"{i}: {district.name}, cost to destroy: {destruction_cost}")
+        district_index = poll_for_index(possible_districts, "district")
+        district = possible_districts[district_index]
+        destruction_cost = max(0, district.value - 1)
+
+        target.played_districts.remove(district)
+        self.player.money -= destruction_cost
+        print(
+            f"{self.player.player_name} destroyed {target.player_name}'s "
+            f"{district.name} for {destruction_cost} gold."
+        )
+
+
+CHARACTER_CLASSES = [
+    Assassin,
+    Thief,
+    Wizard,
+    King,
+    Bishop,
+    Merchant,
+    Architect,
+    Condottiere
+]
 
 
 class CharacterDeck:
     def __init__(self, n_players):
         self.n_players = n_players
         self.visible = []
-        self.characters = [
-            Assassin,
-            Thief,
-            Wizard,
-            King,
-            Bishop,
-            Merchant,
-            Architect,
-            Condottiere
-        ]
+        self.characters = CHARACTER_CLASSES.copy()
+        shuffle(self.characters)
 
-        sel_hidden = np.random.randint(0, len(self.characters), 1)
-        self.hidden = self.characters.pop(sel_hidden)
-        valid = False
-
-        if self.n_player == 7:
-            n_player_picks = 6
-        else:
-            n_player_picks = self.n_players
-        while not valid:
-            sel_visible = np.random.randint(0, len(self.characters), 6 - n_player_picks)
-            if King in np.array(self.characters)[sel_visible]:
+        self.hidden = self.characters.pop()
+        visible_count = self._visible_discard_count()
+        while len(self.visible) < visible_count:
+            character = self.characters.pop()
+            if character is King:
+                self.characters.insert(0, character)
+                shuffle(self.characters)
                 continue
-            for sv in sel_visible:
-                self.visible.append(self.characters.pop(sv))
-            valid = True
+            self.visible.append(character)
+
+    def __len__(self):
+        return len(self.characters)
+
+    def _visible_discard_count(self):
+        if self.n_players == 4:
+            return 2
+        if self.n_players == 5:
+            return 1
+        return 0
+
+    def print_available_characters(self):
+        print("Available characters:")
+        for i, character in enumerate(self.characters):
+            print(f"{i}: {character.name}")
                      
--- game.py
+++ game.py
@@ -8,7 +8,7 @@ def poll_player_for_input(possible_values, poll_type='district'):
     while not valid_input:
         pressed_key = input(poll)
         if pressed_key.isdigit():
-            if int(pressed_key) <= len(possible_values):
+            if int(pressed_key) < len(possible_values):
                 valid_input = True
                 return int(pressed_key)
 
@@ -30,34 +30,39 @@ class Deck:
         return self.cards.pop(0)
 
     def shuffle(self):
-        self.cards = shuffle(self.cards)
-
-
+        shuffle(self.cards)
 
 class Player:
-    def __init__(self, deck, initial_money=2, initial_cards=4, human=True, name=''):
-        self.cards = [deck.draw_card()] * initial_cards
+    def __init__(self, deck, initial_money=2, initial_cards=4, human=True, name='', game=None):
+        self.cards = [deck.draw_card() for _ in range(initial_cards)]
         self.money = initial_money
-        self.available_actions = ['ressources_gathering', 'play_district', 'end_turn']
+        self.available_actions = []
         self.human = human
         self.played_districts = []
         self.character = None
         self.player_name = name
+        self.game = game
+        self.first_finished = False
+        self.city_completed = False
+
+    def __str__(self):
+        return self.player_name
 
     def pick_character(self, character_deck):
+        character_deck.print_available_characters()
         pressed_key = poll_player_for_input(character_deck, 'character')
-        self.character = character_deck.characters.pop(pressed_key)
-        # initialize character
-        self.character = self.character(self)
+        character_class = character_deck.characters.pop(pressed_key)
+        self.character = character_class(self)
+        print(f"{self.player_name} picked {self.character.name}")
 
     def ressource_gathering(self, deck, draw=True):
         if draw:
-            cards_drew = [deck.draw_card()] * 2
+            cards_drew = [deck.draw_card() for _ in range(2)]
             for card in cards_drew:
                 print(card)
             if self.human:
                 pressed_key = poll_player_for_input(cards_drew, 'card_draw')
-                self.cards.append(cards_drew[pressed_key])
+                selected_card = cards_drew[pressed_key]
             else:
                 selected_card = cards_drew[0] # for the moment only the first
             self.cards.append(selected_card)
@@ -73,25 +78,63 @@ class Player:
             print(f"{i}: {dist.name}, {dist.value}, {dist.color}")
         if self.human:
             # Poll to get playable district
-            sel_dist_index = poll_player_for_input(len(possible_districts), poll_type='district')
-            selected_district = self.cards.pop(sel_dist_index)
+            sel_dist_index = poll_player_for_input(possible_districts, poll_type='district')
+            selected_district = possible_districts[sel_dist_index]
+            self.cards.remove(selected_district)
             self.played_districts.append(selected_district)
-            self.money -= selected_district
+            self.money -= selected_district.value
 
     def print_player_status(self):
         print(f"Money: {self.money}")
-        print(f"Cards: {self.cards}")
-        print(f"Played district: {self.played_districts}\n")
+        print(f"Cards:")
+        for c in self.cards:
+            print(f"\t-{c}")
+        print(f"Played district: ")
+        for d in self.played_districts:
+            print(f"\t-{d}")
+        print("\n")
 
     def player_turn(self, deck):
+        if self.game.assassinated == self.character.name:
+            self.game.assassinated = None
+            return
+        if self.game.stolen == self.character.name:
+            thief = [
+            player
+            for player in self.player.game.players
+            if player is not self.player and
+                player.character is not None
+                and player.character.name == "Thief"
+            ][0]
+            thief.money += self.money
+            self.money = 0
+
+        character_actions = [
+            action
+            for action in self.available_actions
+            if isinstance(action, Action)
+        ]
+        self.available_actions = [
+            'ressources_gathering',
+            'play_district',
+            *character_actions,
+            'end_turn'
+        ]
         while len(self.available_actions) > 0:
             print("Available action remaining")
             for i, act in enumerate(self.available_actions):
                 print(f"{i} : {act}")
-            pressed_key = poll_player_for_input(self.available_actions)
+            pressed_key = poll_player_for_input(self.available_actions, "type of action")
             played_action = self.available_actions.pop(pressed_key)
-            if played_action == 'ressources_gathering':
-                self.ressource_gathering(deck)
+            if isinstance(played_action, Action):
+                played_action.play()
+            elif played_action == 'ressources_gathering':
+                ress_gath_actions = ["Draw", "Money"]
+                for i, act in enumerate(ress_gath_actions):
+                    print(f"{i}: {act}")
+                draw_key = poll_player_for_input(ress_gath_actions, 'select ressource gathering type')
+                print(draw_key)
+                self.ressource_gathering(deck, ress_gath_actions[draw_key] == 'Draw')
             elif played_action == "play_district":
                 self.play_district()
             elif played_action == 'end_turn':
@@ -102,31 +145,52 @@ class Player:
             self.print_player_status()
 
 class Game:
-    def __init__(self, n_players=4):
+    def __init__(self, n_players=2):
         self.n_players = n_players
-        cards = [Card(c['name'], c['value'], c['color']) for c in DECK_CONFIGS['base']]
+        card_types_by_name = {card_type['name']: card_type for card_type in CARD_TYPES}
+        cards = [
+            Card(card_type['name'], card_type['value'], card_type['color'])
+            for card_name, count in DECK_CONFIGS['base'].items()
+            for card_type in [card_types_by_name[card_name]]
+            for _ in range(count)
+        ]
         self.deck = Deck(cards)
         self.deck.shuffle()
-        self.players = [Player(self.deck, name=f"Player {i+1}") for i in range(self.n_players)]
+        self.players = [
+            Player(self.deck, name=f"Player {i+1}", game=self)
+            for i in range(self.n_players)
+        ]
+        self.game_finished = False
 
     def game_turn(self):
         char_deck = CharacterDeck(self.n_players)
         for player in self.players:
+            print(f"{player} must pick a character")
             player.pick_character(char_deck)
+            print('\n')
 
-        player_order = [player.character.play_order for player in self.players]
+        players_in_turn_order = sorted(
+            self.players,
+            key=lambda player: player.character.play_order
+        )
+        for player in players_in_turn_order:
+            print(f"{player.character.name}: {player.player_name}'s turn")
+            player.player_turn(self.deck)
+            if len(player.played_districts) >= 7:
+                player.city_completed = True
+                if not self.game_finished:
+                    self.game_finished = True
+                    player.first_finish = True
 
+    def play_game(self):
+        while not self.game_finished:
+            self.game_turn()
+        
         
 
 def main():
-
-    print(deck.draw_card())
-    # while game_not_ended:
-    for i in range(10):
-        player1 = Player(deck)
-        player1.player_turn(deck)
-
-    print(deck)
+    game = Game()
+    game.game_turn()
 
             
 if __name__ == '__main__':
